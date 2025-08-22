f0d388522455a1a61f88aecf1d4c155c1e3aa282
Pavel Galante <pavelgalante@gmail.com>
2025-08-22 10:17:19 +0300

    db: add HomepageConfig table for homepage settings

---
+++ prisma/migrations/20250822_add_homepage_config/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "HomepageConfig" (
+    "id" INTEGER NOT NULL DEFAULT 1,
+    "data" JSONB NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "HomepageConfig_pkey" PRIMARY KEY ("id")
+);
+
+
